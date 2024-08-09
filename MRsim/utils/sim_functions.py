03f514b2c763eceb539c5f5d66224a17dcb392bd
Sebastian Rios <sebas.riosq99@gmail.com>
2024-08-08 21:23:21 -0400

    Updated to working notebooks
    Created new nb for running when we want gauss simulation.
    Need to delete the gaussian parts from the sim nb
    Added correction to sim_functions to work with gauss distributed (maybe optimize later)

--- MRsim/utils/sim_functions.py
+++ MRsim/utils/sim_functions.py
@@ -22,7 +22,15 @@ def optimize_measurement(pd_vol, t2s_vol, dims, deltaB0, FA,TE,B0):
 
     for te_idx, TE_val in enumerate(TE):
         print(f"Processing TE[{te_idx}] = {TE_val}")
-        mag, phase_arr = optimized_signal3(pd_vol, t2s_vol, FA, TE_val, deltaB0, gamma_rd_sT, B0, handedness)
+        # if TE_val >= 1:
+            # Means that its in seconds
+            #mag, phase_arr = sim_with_texture(pd_vol,t2s_vol,FA,TE_val,deltaB0, gamma_rd_sT, B0, handedness)
+        #elif TE_val < 1 and TE_val > 0:
+            # Means its in milisendos
+        mag, phase_arr = sim_with_texture(pd_vol, t2s_vol, FA, TE_val, deltaB0, gamma_rd_sT, B0, handedness)
+        #else:
+            #print("Echo time must be positive!")
+
         print(f"mag shape: {mag.shape}, phase_arr shape: {phase_arr.shape}")
         if mag.shape != tuple(dims):
             raise ValueError(f"Shape mismatch: expected {tuple(dims)} but got {mag.shape}")
@@ -68,7 +76,7 @@ def optimized_signal2(pd_vol,T2star_vol, FA, te, deltaB0_vol, gamma, handedness)
 def optimized_signal3(pd_vol,T2star_vol, FA, te, deltaB0_vol, gamma, B0, handedness):
     # This is an optimized version from generate_signal, using numpy array matrices
     print("Starting optimized_signal")
-    decay = np.exp(-te  / (T2star_vol * 1e-3))  # Convert T2s values to same as Echo Times and apply decay exponential
+    decay = np.exp(- te  / (T2star_vol * 1e-3))  # Convert T2s values to same as Echo Times and apply decay exponential
     fa = np.deg2rad(FA)
     print("sin: ", np.sin(fa))
     
@@ -91,6 +99,51 @@ def optimized_signal3(pd_vol,T2star_vol, FA, te, deltaB0_vol, gamma, B0, handedn
     # Phase factor in radians
 
     signal = pd_vol * np.sin(fa) * decay * np.exp(phase_factor)
+    print("Finished optimized_signal")
+
+    return np.abs(signal), np.angle(signal) # Abs for the Magnitude whereas angle for Phase
+
+# This function is used when Echo Times are in seconds
+def sim_with_texture(pd_vol,T2star_vol, FA, te, deltaB0_vol, gamma, B0, handedness):
+    # This is an optimized version from generate_signal, using numpy array matrices
+    print("Starting optimized_signal")
+    
+    # In the simulation with Gaussian distribution we encounter an overflow on decay
+    # To overcome this issue we can split it into dimensions
+    #decay = np.exp(- te  / (T2star_vol ))  # Convert T2s values to same as Echo Times and apply decay exponential
+    dims = np.array(pd_vol.shape)
+    decay_gauss = np.zeros(dims)
+    
+    for i in range(dims[0]):
+        for j in range(dims[1]):
+            for k in range(dims[2]):
+                decay_gauss[i,j,k] = np.exp(-te / T2star_vol[i,j,k])
+    
+    #for i in range(len(dims)):
+    #    decay_gauss[i] = np.exp(-te / T2star_vol[i])
+    
+    fa = np.deg2rad(FA)
+    print("sin: ", np.sin(fa))
+    
+    if handedness == 'left':
+
+        print('handedness=left')
+        phase_factor = -1j * gamma * deltaB0_vol * B0 * 1e-6 * te * 1e-3
+        coef = -1j * gamma * B0 * 1e-6 * te
+        print("Coefficient of phase factor: ", coef)
+
+    elif handedness == 'right':
+
+        print('handedness=right')
+        phase_factor =  1j * gamma * deltaB0_vol * B0* 1e-6 * te * 1e-3
+        coef = 1j * gamma * B0 * 1e-6 * te
+        print("Coefficient of phase factor: ", coef)
+
+    else:
+        print('wrong handedness')
+    # Phase factor in radians
+
+    signal = pd_vol * np.sin(fa) * decay_gauss * np.exp(phase_factor)
     print("Finished optimized_signal")
 
     return np.abs(signal), np.angle(signal) # Abs for the Magnitude whereas angle for Phase
