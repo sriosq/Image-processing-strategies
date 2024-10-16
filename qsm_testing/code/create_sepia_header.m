9b255ed758d0a232a718470a246ae808f91e0c36
Sebastian Rios <sebas.riosq99@gmail.com>
2024-10-15 23:09:12 -0400

    Update to notebooks on stratz
    Adding the compare fm notebook to the compare_fm (new project)

--- qsm_testing/code/create_sepia_header.m
+++ qsm_testing/code/create_sepia_header.m
@@ -12,7 +12,7 @@ voxelSize = [0.976562, 0.976562, 2.344]; % Pixdim
 save("header_mrsim.mat",'B0','CF','B0_dir',"TE", "matrixSize","voxelSize");
 
 %%
-% Values for GRE acq from23-08
+% Values for GRE acq from 23-08 // db0-030
 B0 = 3; % Magnetic Field Strength in Tesla
 B0_dir = [0;0;1]; % Direction of B0
 CF = 127740000; % Central Frequency
@@ -22,3 +22,14 @@ matrixSize = [144, 144, 20]; % The "dimensions" of the image used
 voxelSize = [1.9792, 1.9792, 2]; % Pixdim 
 
 save("header_torso.mat",'B0','CF','B0_dir',"TE", "matrixSize","voxelSize");
+
+%% 
+B0 = 3;
+B0_dir = [0;0;1];
+CF = 127740000;
+TE = [0.0033 0.0052];
+% Echo time or list of echo times
+matrixSize = [192, 320, 320]; % The "dimensions" of the image used
+voxelSize = [1, 1, 1]; % Pixdim 
+
+save("2choes_dB0_030.mat",'B0','CF','B0_dir',"TE", "matrixSize","voxelSize");
