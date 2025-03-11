e51242f3f2977f0c9beae5bbee3ceefab2d63d21
Sebastian Rios <sebas.riosq99@gmail.com>
2025-03-11 08:56:12 -0400

    Created scripts and connection to MATLAB works
    Exact same results as running SEPIA GUI, next step is setting up the objective function

--- bgfr_fine_tuner/python_to_matlab_wrap.py
+++ bgfr_fine_tuner/python_to_matlab_wrap.py
@@ -2,58 +2,62 @@
 import PyNomad as nomad
 import matlab.engine
 import os
-eng = matlab.engine.start_matlab()
 
-sepia_path = "D:/Poly_MSc_Code/libraries_and_toolboxes/sepia"
-xtra_tb_path = "D:/Poly_MSc_Code/libraries_and_toolboxes/toolboxes"
+def python_to_sepia_bfr_wrap(input_fieldmap, in2, in3, sepia_header, alg, params, out_basename, mask_filename ):
+    ''' This is a wrapper for Sepia used to create BGFR algorithms
+    '''
+    eng = matlab.engine.start_matlab()
 
-eng.addpath(sepia_path)
-bfr_wrappers = eng.genpath("D:/Poly_MSc_Code/libraries_and_toolboxes/sepia/wrapper")
-eng.addpath(bfr_wrappers, nargout=0)
+    sepia_path = "D:/Poly_MSc_Code/libraries_and_toolboxes/sepia"
+    xtra_tb_path = "D:/Poly_MSc_Code/libraries_and_toolboxes/toolboxes"
 
-all_funcs = eng.genpath("D:/Poly_MSc_Code/libraries_and_toolboxes/sepia")
-eng.addpath(all_funcs, nargout=0)
+    eng.addpath(sepia_path)
+    bfr_wrappers = eng.genpath("D:/Poly_MSc_Code/libraries_and_toolboxes/sepia/wrapper")
+    eng.addpath(bfr_wrappers, nargout=0)
 
-path_to_MEDI_tb = "D:/Poly_MSc_Code/libraries_and_toolboxes/toolboxes/MEDI_toolbox"
-medi_sama = eng.genpath(path_to_MEDI_tb)
-eng.addpath(medi_sama, nargout = 0)
+    all_funcs = eng.genpath("D:/Poly_MSc_Code/libraries_and_toolboxes/sepia")
+    eng.addpath(all_funcs, nargout=0)
+
+    path_to_MEDI_tb = "D:/Poly_MSc_Code/libraries_and_toolboxes/toolboxes/MEDI_toolbox"
+    medi_sama = eng.genpath(path_to_MEDI_tb)
+    eng.addpath(medi_sama, nargout = 0)
 
 
-#  LBV Parameters
-tolerance = 0.0001
-depth = 5
-peel = 2
+    #  LBV Parameters
+    tolerance = 0.0001
+    depth = 5
+    peel = 2
 
-bfr_params = {  # Example method name
+    bfr_params = {  # Example method name
     
-    'general' : {
-        'isBET' : '0',
-        'isInvert':'0',
-        'isRefineBrainMask' : '0'
-    },
-    'bfr':{
-    'method': "LBV",
-    'tol': float(tolerance),
-    'depth': float(depth),
-    'peel': float(peel),
-    "refine_method" : "None",
-    "refine_order" : 4,
-    'erode_radius': 0,
-  'erode_before_radius': 0}
+        'general' : {
+            'isBET' : '0',
+            'isInvert':'0',
+            'isRefineBrainMask' : '0'
+        },
+        'bfr':{
+        'method': "LBV",
+        'tol': float(tolerance),
+        'depth': float(depth),
+        'peel': float(peel),
+        "refine_method" : "None",
+        "refine_order" : 4,
+        'erode_radius': 0,
+    'erode_before_radius': 0}
+    }
+
+    matrixSize = [101,171,141]
+    voxelSize = [0.9766, 0.9766, 2.3440]
+
+    output_basename =  str("E:\msc_data\sc_qsm\data\cropped\piece-wise\simulation\TE_1_weird_40/bgfr_sepia/something/Sepia")
+    mask_filename = str("E:\msc_data\sc_qsm\data\cropped\piece-wise\simulation\canal_crop2.nii.gz")
+
+    in1 = "E:\msc_data\sc_qsm\data\cropped\piece-wise\simulation\TE_1_weird_40/romeo_tests/test3_right_masked\B0.nii"
+    in2 = ""
+    in3 = ""
+    in4 = "E:/msc_data/sc_qsm/data/cropped/piece-wise/simulation/TE_1_weird_40/header_qsm_tsting_hcrop2.mat"
 
 
-}
-
-matrixSize = [101,171,141]
-voxelSize = [0.9766, 0.9766, 2.3440]
-
-output_basename =  str("E:\msc_data\sc_qsm\data\cropped\piece-wise\simulation\TE_1_weird_40/bgfr_sepia/something/Sepia")
-mask_filename = str("E:\msc_data\sc_qsm\data\cropped\piece-wise\simulation\canal_crop2.nii.gz")
-
-in1 = "E:\msc_data\sc_qsm\data\cropped\piece-wise\simulation\TE_1_weird_40/romeo_tests/test3_right_masked\B0.nii"
-in2 = ""
-in3 = ""
-in4 = "E:/msc_data/sc_qsm/data/cropped/piece-wise/simulation/TE_1_weird_40/header_qsm_tsting_hcrop2.mat"
-
-
-eng.python_wrapper(in1, in2, in3, in4 , output_basename, mask_filename, bfr_params, nargout = 0)
+    eng.python_wrapper(in1, in2, in3, in4 , output_basename, mask_filename, bfr_params, nargout = 0)\
+    # Close matlab engine :)
+    eng.quit()
